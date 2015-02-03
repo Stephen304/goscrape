1efc7a11c3169b4d79388ab887949849a34038ce
Stephen Smith <stephen304@gmail.com>
2015-02-03 18:11:05 -0500

    Watch the expire time and refresh sessions when IDs expire.

--- goscrape.go
+++ goscrape.go
@@ -1,9 +1,12 @@
 package goscrape
 
-import ()
+import (
+	"time"
+)
 
 type Bulk struct {
-	Sess []Session
+	Sess   []Session
+	Expire time.Time
 }
 
 func NewBulk(trackers []string) Bulk {
@@ -20,10 +23,13 @@ func NewBulk(trackers []string) Bulk {
 		sessions[i] = <-channels[i]
 	}
 
-	return Bulk{sessions}
+	return Bulk{Sess: sessions, Expire: time.Now().Add(1 * time.Minute)}
 }
 
-func (bulk Bulk) ScrapeBulk(btihs []string) []Result {
+func (bulk *Bulk) ScrapeBulk(btihs []string) []Result {
+	if time.Now().After(bulk.Expire) {
+		bulk.refreshSessions()
+	}
 	var results []Result = make([]Result, len(btihs))
 	for i := 0; i < len(results); i++ {
 		results[i] = Result{btihs[i], 0, 0, 0}
@@ -51,4 +57,24 @@ func (bulk Bulk) ScrapeBulk(btihs []string) []Result {
 
 func asyncSession(url string, output chan Session) {
 	output <- NewConn(url)
+}
+
+func (bulk *Bulk) refreshSessions() {
+	// Get the size of the sessions
+	size := len(bulk.Sess)
+	// Make channels
+	var channels = make([]chan Session, size)
+
+	// Make channels and make new sessions asynchronously
+	for i := 0; i < size; i++ {
+		channels[i] = make(chan Session)
+		go asyncSession(bulk.Sess[i].URL, channels[i])
+	}
+
+	// Replace old sessions with new ones
+	for i := 0; i < size; i++ {
+		bulk.Sess[i] = <-channels[i]
+	}
+
+	bulk.Expire = time.Now().Add(1 * time.Minute)
 }
